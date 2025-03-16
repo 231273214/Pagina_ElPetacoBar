46e594c96a14f6e9d16ee0b0c6b307cb9eccae6d
BautistaMabel <mdbautista@uniboyaca.edu.co>
2025-03-16 02:30:43 -0500

    Se modifica página principal y se agregan nuevos archivos

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Seleccionar el dropdown
     let dropdown = document.querySelector(".dropdown");
     let dropdownMenu = document.querySelector(".dropdown-menu");
 
-    // Mostrar menú al pasar el mouse
     dropdown.addEventListener("mouseenter", function () {
         dropdownMenu.classList.add("show");
     });
-
-    // Ocultar menú al salir
     dropdown.addEventListener("mouseleave", function () {
         dropdownMenu.classList.remove("show");
     });
-
-    // Detectar scroll y cambiar el navbar
     let navbar = document.querySelector(".navbar");
     window.addEventListener("scroll", function () {
         requestAnimationFrame(() => {
